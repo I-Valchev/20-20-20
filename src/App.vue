079b56855ea49c02a6c420e891ad7602680efe0d
Ivo Valchev <ivo.v.valchev@gmail.com>
2020-12-06 21:17:32 +0100

    UI improvements. How? and Why?. GA

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,44 @@
 <template>
-<!--  <img alt="Vue logo" src="./assets/logo.png">-->
   <div class="container">
     <p>Take regular 20-second breaks, every 20 minutes.</p>
     <Countdown :milliseconds=state.milliseconds @completed="this.handleCompleted"  />
 
     <div>
       <button @click="this.handleStop">{{ state.state === State.STOPPED ? 'Restart' : 'Stop' }}</button>
-      <button @click="this.handleSilence">Silence for 1 hour</button>
+      <button @click="this.handleSilence" class="secondary">Silence for 1 hour</button>
+    </div>
+  </div>
+
+  <div id="content">
+    <div>
+      <h2>Why?</h2>
+      <p>Humans normally blink about 15 times in one minute. However, studies show that we only blink about 5 to 7
+        times in a minute while using computers and other digital screen devices.
+        Blinking is the eye’s way of getting the moisture it needs on its surface.
+      </p>
+
+      <p>
+        Take regular breaks using the “20-20-20” rule: every 20 minutes, shift your eyes to look at an
+        object at least 20 feet (≈ 6 meters) away, for at least 20 seconds.
+        This rule is recommended by the American Academy of Ophthalmology, the UK National Health Service,
+        The Canadian Association of Optometrists and many other health institutions.
+      </p>
+    </div>
+
+    <div>
+      <h2>How it works?</h2>
+      <p>
+        This app reminds you to take regular 20-second breaks, every 20 minutes. To do so, it sends you
+        notifications every time you need to take a pause, and a reminder to get back to work once
+        the 20 seconds elapse.
+      </p>
+
+      <p>
+        To do so, you need to allow the app to send you notifications. You can check your notifications settings
+        for <a href="https://support.google.com/chrome/answer/3220216" target="_blank">Google Chrome here</a>,
+        or for
+        <a href="https://support.mozilla.org/en-US/kb/push-notifications-firefox" target="_blank">Mozilla Firefox here</a>.
+      </p>
     </div>
   </div>
 </template>
@@ -78,9 +110,23 @@ export default {
 <link href="https://fonts.googleapis.com/css?family=Lato:100,300|PT+Sans:400,700" rel="stylesheet">
 <style>
 
+:root {
+  --main-color: white;
+  --accent-color: #333;
+
+}
+
+@media (prefers-color-scheme: dark){
+  :root {
+    --main-color: #333;
+    --accent-color: white;
+  }
+}
+
 body {
   font-family: "PT Sans", serif;
-  color: #333;
+  color: var(--accent-color);
+  background-color: var(--main-color);
   min-height: 100vh;
   display: flex;
   flex-direction: column;
@@ -99,12 +145,13 @@ h1 {
   align-items: center;
   display: flex;
   flex-direction: column;
+  min-height: 80vh;
+  justify-content: center;
 }
 
 button {
-  background-color: #333; /* Green */
-  border: none;
-  color: white;
+  background-color: var(--accent-color);
+  color: var(--main-color);
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;
@@ -114,11 +161,32 @@ button {
   margin: 1em;
   min-width: 10em;
   text-transform: uppercase;
+  border: 2px solid var(--accent-color);
+}
+
+button.secondary {
+  background-color: var(--main-color);
+  color: var(--accent-color);
 }
 
 button:hover {
-  background-color: #111;
+  background-color: var(--accent-color);
   transition: all 0.2s ease-in-out;
 }
 
+button.secondary:hover {
+  color: var(--main-color);
+}
+
+@media screen and (min-width: 768px) {
+  body {
+    margin: 1rem 10rem;
+  }
+}
+
+#content {
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(auto-fit, minmax(300px , 1fr));
+}
 </style>
